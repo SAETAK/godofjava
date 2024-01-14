af708ab978c345e15621a7eb4a510624c012dd17
SAETAK <ihjang404@gmail.com>
2024-01-15 00:37:12 +0900

    ! operator

---
+++ OperatorComplement.java
@@ -0,0 +1,12 @@
+public class OperatorComplement {
+    public static void main(String args[]) {
+        OperatorComplement sample=new OperatorComplement();
+        sample.complement();
+    }
+
+    public void complement() {
+        boolean flag=true;
+        System.out.println(flag);
+        System.out.println(!flag);
+    }
+}
